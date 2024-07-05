63954f2b5f366f89990061e0f8b2593829c97120
renatapnunes <renatanunes231@yahoo.com.br>
2024-07-05 18:35:08 -0300

    feat: add styled-components and create theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,20 @@
 import {StrictMode} from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { ThemeProvider } from 'styled-components'
 
 import App from './App.tsx'
 
+import theme from './styles/theme'
+import { GlobalStyle } from './styles/globalStyle'
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
   </StrictMode>,
 )
---
+++ src/styles/theme.ts
@@ -0,0 +1,39 @@
+export default {
+  palette: {
+    brandWhite: {
+      main: '#FFFFFF',
+    },
+    brandGrey: {
+      background: '#343434',
+      lightest: '#9F9F9F',
+      lighter: '#5F5F5F',
+      light: '#494949',
+      main: '#313131',
+      dark: '#373737',
+      darker: '#2B2B2B',
+      darkest: '#252525',
+    },
+    brandGreen: {
+      main:  '#71BB00',
+    },
+    brandRed: {
+      main: '#D65924',
+    }
+  },
+  typography: {
+    fontFamily: 'Inter, sans-serif',
+    fontSize: {
+      small: '10px',
+      medium: '11px',
+      large: '12px',
+      extraLarge: '22px',
+    }
+  },
+  breakpoints: {
+    xs: 0,
+    sm: 600,
+    md: 900,
+    lg: 1200,
+    xl: 1536,
+  },
+}
